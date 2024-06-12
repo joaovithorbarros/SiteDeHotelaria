f9fe89075a0645f2a4bba2ff0f1b23419d32ebef
Lumero0 <joaovithorbmv@gmail.com>
2024-06-12 19:17:27 -0300

    SECE: Version 7.5

--- cadastro.php
+++ cadastro.php
@@ -1,29 +1,43 @@
-<h1>Cadastro</h1>
 <?php
     include("config.php");
 ?>
-<link rel="stylesheet" href="./CSS/bootstrap.min.css">
-<form action="salvar.php" method="POST">
-    <input type="hidden" name="acao" value="cadastrar">
-    <div class="mb-3">
-        <label for="">Nome</label>
-        <input type="text" name="nome" class="form-control" required>
-    </div>
-    <div class="mb-3">
-        <label for="">E-mail</label> 
-        <input type="text" name="email" class="form-control" required>
-    </div>
-    <div class="mb-3">
-        <label for="">Senha</label> 
-        <input type="password" name="senha" class="form-control" required>
-    </div>
-    <div class="mb-3">
-        <label for="">Data de Nascimento</label>
-        <input type="date" name="data-nasc" class="form-control" required>
-    </div>
-    <div class="mb-3">
-        <button type="submit" class="btn btn-primary">Enviar</button>
-    </div>
-</form>
 
+<!DOCTYPE html>
+<html lang="pt-BR" class="h-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="./CSS/form.css">
+    <title>Cadastro</title>
+</head>
+<body class="d-flex align-items-center py-4 bg-body-tertiary h-100">
+    <main class="w-100 m-auto form-container-cad">
+        <form action="salvar.php" method="POST" class="text-center">
+            <div class="text-center">
+                <input type="hidden" name="acao" value="cadastrar">
+                <img src="./IMAGENS/SECE.png" class="mb-4" height="150" width="250" />
+            </div>
+            <h1 class="h3 mb-3 fw-normal">Realizar cadastro</h1>
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput" placeholder="your-email@gmail.com" name="nome" required>
+            <label for="floatingInput">Nome</label> 
+            </div>
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="your-email@gmail.com" name="email">
+            <label for="floatingInput">E-mail</label required>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingInput" placeholder="your-email@gmail.com" name="senha" required>
+            <label for="floatingInput">Senha</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="date" class="form-control" id="floatingInput" placeholder="your-email@gmail.com" name="data-nasc" required>
+            <label for="floatingInput">Data de Nascimento</label>
+            </div>
+            <button class="btn btn-primary w-50 py-2 ">Entrar</button>
+        </form>
+    </main>
 <script src="./JS/bootstrap.bundle.min.js"></script>
+</body>
+</html>
